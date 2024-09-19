c5bf084687c1d752b03f9088f356dfff9273d5f5
suyadeen <suyabubbloo94@gmail.com>
2024-09-19 12:03:04 +0530

    Add files via upload
    second task

---
+++ Task-2 ALPHABET(CHR( ) ).py
@@ -0,0 +1,27 @@
+'''n=1
+for i in range(0,5,1):
+    j=0
+    while(j<i):
+        print("*",end= ' ')
+        j+=1
+    print("\n")'''
+
+
+for i in range(65,70,1):
+    for j in range(65,i+1,1):
+        print(chr(j),end= ' ')
+    print ()
+
+
+        
+
+
+
+
+
+
+
+
+
+        
+     
